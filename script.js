226f462ca26cff9477dd1e0be59c12497e8510ec
Winfredo <winfrednukpe2002@gmail.com>
2023-01-31 17:58:25 +0000

    Changes

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let myLeads = [ "www.awesome.com","www.google.com", "www.gana.com"]
-const Input = document.getElementById("input-btn")
-const inputEl = document.getElementById("input-el")
-const UlEl = document.getElementById("ul-el")
+let myLeads = [];
+const Input = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const UlEl = document.getElementById("ul-el");
 
-Input.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-}) 
+Input.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
 
-for(i = 0; i < myLeads.length; i++ ){
-    UlEl.innerHTML  += "<li>" + myLeads[i]  + "</li>"
-    console.log(myLeads[i])
+function renderLeads() {
+  let ListItems = "";
+
+  for (i = 0; i < myLeads.length; i++) {
+    ListItems += `
+    <li>
+        <a target='blank' href='${myLeads[i]}'> ${myLeads[i]}</a>
+        </li>
+        `;
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // UlEl.append(li)
+  }
+
+  UlEl.innerHTML = ListItems;
 }
-
