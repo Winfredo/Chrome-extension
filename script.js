b4f849ff1af2e51d99fde1185b39cc9b03a4f69e
Winfredo <winfrednukpe2002@gmail.com>
2023-02-02 15:22:02 +0000

    Changes

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const Input = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const UlEl = document.getElementById("ul-el");
 
+// myLeads = JSON.parse(myLeads)
+// localStorage.getItem("myLeads")
+
+// const leadsfromLocalStorage = myLeads
+
+let leadsfromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsfromLocalStorage)
+  
+
+
 Input.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
-  renderLeads();
+
+ localStorage.setItem("myLeads", JSON.stringify(myLeads))
+ renderLeads();
+
+
+  console.log(localStorage.getItem("myLeads"))
 });
 
 function renderLeads() {
@@ -15,9 +30,11 @@ function renderLeads() {
   for (i = 0; i < myLeads.length; i++) {
     ListItems += `
     <li>
-        <a target='blank' href='${myLeads[i]}'> ${myLeads[i]}</a>
-        </li>
+        <a target='blank' href='${myLeads[i]}'> ${myLeads[i]}
+        </a>
+      </li>
         `;
+
     // const li = document.createElement("li")
     // li.textContent = myLeads[i]
     // UlEl.append(li)
